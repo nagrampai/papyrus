98bf490cc7e13ee78f20110e89ac756e684a9531
nagrampai <nageshpai@gmail.com>
2022-09-26 10:43:01 +0530

    Add forge and minor updates

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require( 'electron' );
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -11,22 +11,22 @@ const createWindow = () => {
     },
   });
 
-  win.loadFile("src/index.html");
+  win.loadFile( "src/index.html" );
 };
 
 app.whenReady().then(() => {
 
   createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+  app.on( 'activate', () => {
+		if ( BrowserWindow.getAllWindows().length === 0 ) {
+			createWindow();
+		}
+  } );
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on( 'window-all-closed', () => {
+  if ( process.platform !== 'darwin' ) {
     app.quit();
   }
-});
+} );
--- src/js/check-book.js
+++ src/js/check-book.js
@@ -1,42 +1,48 @@
-var connection = require('./js/db');
+var connection = require( './js/db' );
 
-const bookSearchForm = document.querySelector('form');
-const resultDiv = document.querySelector('#resultDiv');
+const bookSearchForm = document.querySelector( 'form' );
+const resultDiv = document.querySelector( '#resultDiv' );
 
-function getBookId(event){
+function getBookId( event ){
   event.preventDefault();
-  const bookNum = bookSearchForm.elements['book-id'].value;
+  const bookNum = bookSearchForm.elements[ 'book-id' ].value;
 
   if( connection.state === 'disconnected' ){
-  connection.connect( (error) => {
-    if(error) throw error;
-    console.log('connected');
+  connection.connect( ( error ) => {
+    if( error ) throw error;
+    console.log( 'connected' );
   });}
  
   let sqlQuery = "";
-  if( bookNum.length === 5 && (bookNum[0] === 'G' ||bookNum[0] === 'K' )){
+  if( bookNum.length === 5 && ( bookNum[0] === 'G' || bookNum[0] === 'K' )){
     sqlQuery = 'SELECT * FROM books WHERE book_code= "' + bookNum + '";';
   }
    else sqlQuery = 'SELECT * FROM books WHERE book_id=' + bookNum + '\;';
  
 
-   connection.query(sqlQuery, function (error, results, fields) {
-     if (error) {
+   connection.query( sqlQuery, function ( error, results, fields ) {
+     if ( error ) {
        handleError();
        throw error;
      } 
-     else displayBookResult(results[0]);
-    });
+     else displayBookResult( results[0] );
+    } );
     
   }
   
-function displayBookResult(bookData) {
+function displayBookResult( bookData ) {
     console.log( bookData );
-    const booksContent = `
-          Book ID: ${bookData['book_id']} <br />
-          Book Code: ${bookData['book_code']} <br />
-          Title: ${bookData["title"]} <br />
-          Author: ${bookData["author"]} <br />`;
+    let booksContent = `
+          Book ID  : ${ bookData[ 'book_id' ] } <br />
+          Book Code: ${ bookData[ 'book_code' ] } <br />
+          Title    : ${ bookData[ "title" ] } <br />
+          Author   : ${ bookData[ "author" ] } <br />`;
+
+    if( bookData[ 'available' ] === 1 ) {
+      booksContent += 'The book is currently available for issue';
+    } else {
+      booksContent += ' The book is currently not available';
+    }
 
     resultDiv.innerHTML = booksContent;
   }
@@ -45,4 +51,4 @@ function handleError(){
   resultDiv.innerHTML = `<p>Unable to locate book in our records. Please check the ID</p>`;
 }
 
-bookSearchForm.addEventListener('submit', getBookId);
+bookSearchForm.addEventListener( 'submit', getBookId );
