4a76965fe56c361a0130184fe06c4048018975a4
Nagesh Pai <nageshpai@gmail.com>
2023-04-02 00:40:00 +0530

    test changes

--- src/js/member-master.js
+++ src/js/member-master.js
@@ -1,14 +1,14 @@
-const { runDBQuery } = require( '../js/db' );
-const { getMemberIDFromFlatNumber } = require( '../js/members' );
+const { runDBQuery } = require('../js/db');
+const { getMemberIDFromFlatNumber } = require('../js/members');
 
-const checkMemberButton = document.getElementById( 'check-member-button' );
+const checkMemberButton = document.getElementById('check-member-button');
 const memberFlatNumberField = document.getElementById(
     'member-flat-number-textbox'
 );
-const memberNameField = document.getElementById( 'member-name-textbox' );
-const editMemberButton = document.getElementById( 'add-edit-member-button' );
+const memberNameField = document.getElementById('member-name-textbox');
+const editMemberButton = document.getElementById('add-edit-member-button');
 
-checkMemberButton.addEventListener( 'click', checkMemberButtonHandler );
+checkMemberButton.addEventListener('click', checkMemberButtonHandler);
 
 /**
  * Manage member search event, and query details from DB
@@ -19,14 +19,14 @@ checkMemberButton.addEventListener( 'click', checkMemberButtonHandler );
 function checkMemberButtonHandler() {
     const memberFlatNumber = memberFlatNumberField.value;
 
-    if ( memberFlatNumber.length !== 5 ) {
-        alert( 'Please enter a valid flat number' );
+    if (memberFlatNumber.length !== 5) {
+        alert('Please enter a valid flat number');
         return;
     }
-    const memberID = getMemberIDFromFlatNumber( memberFlatNumber );
+    const memberID = getMemberIDFromFlatNumber(memberFlatNumber);
 
     const sqlQuery = `SELECT * FROM members WHERE member_id = ${memberID}`;
-    runDBQuery( sqlQuery, editMemberDetails );
+    runDBQuery(sqlQuery, editMemberDetails);
 
     /**
      * Display member details if member exists in DB or allow to add member
@@ -34,18 +34,18 @@ function checkMemberButtonHandler() {
      * @param {Object} memberDetails
      */
 
-    function editMemberDetails( memberDetails ) {
-        if ( memberDetails.length > 0 ) {
+    function editMemberDetails(memberDetails) {
+        if (memberDetails.length > 0) {
             const memberName = memberDetails[0].name;
             //const memberRemarks = memberDetails[0].remarks;
             memberNameField.value = memberName;
         } else {
             memberNameField.value = '';
-            alert( 'Member not found, create new?' );
+            alert('Member not found, create new?');
         }
         editMemberButton.hidden = false;
         editMemberButton.disabled = false;
-        editMemberButton.addEventListener( 'click', addMemberHandler );
+        editMemberButton.addEventListener('click', addMemberHandler);
     }
 
     /**
@@ -53,37 +53,39 @@ function checkMemberButtonHandler() {
      *
      * @param {Object} event
      */
-    function addMemberHandler( event ) {
+    function addMemberHandler(event) {
         event.preventDefault();
 
         const newMemberName = memberNameField.value;
         const addMemberQuery = `INSERT INTO library.members ( members.member_id, members.name) VALUES('${memberID}', '${newMemberName}' );`;
-        runDBQuery( addMemberQuery, () => {
-            alert( 'Member added!' );
+        runDBQuery(addMemberQuery, () => {
+            alert('Member added!');
             editMemberButton.hidden = true;
             editMemberButton.disabled = true;
             memberNameField.value = '';
             memberFlatNumberField.value = '';
-        } );
+        });
     }
+
+    console.log(memberID);
 
     /**
      * Edit member details handler
      *
      * @param {Object} event
      */
-    function editMemberHandler( event ) {
+    function editMemberHandler(event) {
         event.preventDefault();
 
-        if ( window.confirm( 'Are you sure you want to update member details?' ) ) {
+        if (window.confirm('Are you sure you want to update member details?')) {
             const updateMemberQuery = `UPDATE library.members SET name = '${
                 memberNameField.value
             }' WHERE member_id = '${memberID.toString()}';`;
-            runDBQuery( updateMemberQuery, () => {
-                alert( 'Member details updated!' );
+            runDBQuery(updateMemberQuery, () => {
+                alert('Member details updated!');
                 editMemberButton.hidden = true;
                 editMemberButton.disabled = true;
-            } );
+            });
         }
     }
 }
