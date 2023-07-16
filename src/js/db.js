e12773778da0b0f0db951640a40b727123aca723
Nagesh Pai <nageshpai@gmail.com>
2023-07-16 22:48:49 +0530

    edit version number

--- src/js/db.js
+++ src/js/db.js
@@ -2,11 +2,13 @@ require( 'dotenv' ).config();
 const mysql = require( 'mysql' );
 const { default: Swal } = require('sweetalert2');
 
+console.log( process.env.DB_PWD, process.env.DB_USERNAME, process.env.DB_NAME );
+
 const connection = mysql.createConnection( {
     host: 'localhost',
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
+    user: 'athenalibrary',
+    password: 'athena@123$',
+    database: 'library'
 } );
 
 function startConnection() {
@@ -14,6 +16,7 @@ function startConnection() {
     try {
       if (connection.state === 'disconnected') {
         connection.connect();
+        console.log( connection );
       }
     } catch (error) {
       throw error;
